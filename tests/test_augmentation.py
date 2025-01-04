2306b2f1ac1ef0f08e020b5361f977c05764e646
Austin Odom <aodom4@una.edu>
2025-01-03 23:12:18 -0600

    Updated the test augmentation file.
    Made a change to view the result output through the terminal.

--- tests/test_augmentation.py
+++ tests/test_augmentation.py
@@ -14,11 +14,18 @@ class TestCombineContext(unittest.TestCase):
         self.assertIn("Question: This is a test query.", result)
         self.assertIn("Context: This is a returned test abstract.", result)
 
+        # Print the result for viewing.
+        print(f"{result}\n\n")
+        print()
+
     def test_with_empty_abstracts(self):
         query = "Some bogus query."
         abstracts = []
         result = combine_context(query, abstracts)
         self.assertEqual(result, "Question: Some bogus query.\nContext: No relevant information was found.")
+
+        # Print the result for viewing.
+        print(f"{result}\n\n")
 
 if __name__ == "__main__":
     unittest.main()
